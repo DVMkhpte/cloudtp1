73877bf16881d1cdf9b32f2f4f9833de385e7f27
DVM_khpte <147807946+DVMkhpte@users.noreply.github.com>
2024-02-27 10:44:43 +0100

    Add bdd

--- esgi.render.nodejs/db.js
+++ esgi.render.nodejs/db.js
@@ -2,8 +2,8 @@ const { Sequelize } = require('sequelize')
 
 // database
 const sequelize = new Sequelize(
-  '', // Database name
-  '', // User
+  DATABASE_NAME, // Database name
+  USER_NAME, // User
   '', // Password
   {
     host: '', // Host
